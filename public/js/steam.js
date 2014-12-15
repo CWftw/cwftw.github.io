f649a9d1f7b0371ebbf9aefab01bbceccea929b0
korin125 <colin.korin@gmail.com>
2014-12-14 20:19:38 -0500

    Update

--- public/js/steam.js
+++ public/js/steam.js
@@ -6,7 +6,7 @@ $(document).ready(function()
 {
     $("#search").click(searchClick);
     
-    $.getJSON("http://api.steampowered.com/ISteamApps/GetAppList/v0001/?callback=?", function (data) {
+    $.getJSON(PROXY + "http://cwftw.site90.net/steam.php?function=allGames", function (data) {
         allGames = data;
         console.log(data);
    }); 
