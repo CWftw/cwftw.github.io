208dae40caad1806dd69c1c92599a3576aed29f0
CWftw <Colin@CWftw.me>
2014-12-22 21:24:42 -0500

    Update

--- public/js/steam.js
+++ public/js/steam.js
@@ -2,73 +2,62 @@ var PROXY = "http://jsonp.nodejitsu.com/?url=";
 var allGames;
 var enteredGames = [];
 
-$(document).ready(function()
-{
+$(document).ready(function () {
     $("#search").click(searchClick);
-    
+
     $.getJSON(PROXY + "http://cwftw.site90.net/steam.php?function=allGames", function (data) {
         allGames = data;
         console.log(data);
-   }); 
+    });
 });
 
-function searchClick()
-{
-    if($("#games").val().length > 0 && $("#profile").val().length > 0)
-    {
-        try
-        {
+function searchClick() {
+    if ($("#games").val().length > 0 && $("#profile").val().length > 0) {
+        try {
             getId($("#profile").val());
             readLines();
-        } catch (ex)
-        {
+        } catch (ex) {
             alert("Invalid profile URL!");
         }
-    } else
-    {
+    } else {
         alert("Invalid input!");
     }
 }
 
-function getId(url)
-{
+function getId(url) {
     $.ajax({
         type: "GET",
         url: url + "?xml=1",
         dataType: "jsonp xml",
-        success: function(xmlResponse) {
+        success: function (xmlResponse) {
             console.log(xmlResponse);
         }
     });
-    
-//        $.getJSON(url + "?xml=1?callback=?", function (data) {
-//            console.log(data);
-//   }); 
-}
-    
 
-function readLines()
-{
+    //        $.getJSON(url + "?xml=1?callback=?", function (data) {
+    //            console.log(data);
+    //   }); 
+}
+
+
+function readLines() {
     var lines = $("#games").val().split('\n');
-    $.each(lines, function(){
+    $.each(lines, function () {
         matchTitle(this);
     });
 }
 
-function matchTitle(title)
-{
+function matchTitle(title) {
     console.log(allGames);
     $.each(allGames.applist.apps.app, function (index, item) {
         console.log(item);
-        if(item.name == title)
-        {
+        if (item.name == title) {
             enteredGames.push(item.appid);
             return false;
         }
     });
 }
 
-function generateTable()
-{
+function generateTable() {
     //
 }
