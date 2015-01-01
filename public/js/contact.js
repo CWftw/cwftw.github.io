9788ebabb7d4c422eb5f4e4f3837ae96a0cf1e5c
Colin Willson <Colin@CWftw.me>
2015-01-01 18:48:27 -0500

    Update

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,5 +1,34 @@
 function validate() {
+	var error = "";
 	var valid = false;
 	
+	var name = document.getElementById("name").value;
+	var email = document.getElementById("email").value;
+	var body = document.getElementById("body").value;
+	
+	if(name.trim() == "" || email.trim() == "" || body.trim() == "")
+	{
+		error += "Please fill in all required fields!";
+	} else
+	{
+		if(validateEmail(email))
+		{
+			return true;
+		} else
+		{
+			error+= "\nEmail is not valid!";
+		}
+	}
+	
+	if(error != "")
+	{
+		valid = false;
+	}
+	
 	return valid;
+}
+
+function validateEmail(email) { 
+    var regex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return regex.test(email);
 }
